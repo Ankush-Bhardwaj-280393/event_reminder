c1151703a8e55c79b19d55ba273eaaf8d2bf2b2e
Ankush Bhardwaj <ankushbhrdwj@gmail.com>
2025-03-13 10:33:30 +0530

    Adding the first draft of working code for the web app

--- app.py
+++ app.py
@@ -8,12 +8,24 @@ birthday_df=pd.read_excel("data/birthdays.xlsx")
 anniversary_df=pd.read_excel("data/anniversaries.xlsx")
 
 def filter_data():
-    pass
+    today_date=datetime.date.today().strftime("%d/%m")
+
+    birthday_df["date_only"]=birthday_df["Birthday"].apply(lambda x: x.strftime("%d/%m"))
+    anniversary_df["date_only"]=anniversary_df["Anniversary"].apply(lambda x: x.strftime("%d/%m"))
+
+    birthday_filter_df=birthday_df[birthday_df["date_only"]==today_date]
+    anniversary_filter_df=anniversary_df[anniversary_df["date_only"]==today_date]
+
+    anniversary_filter_df["pair_name"]=anniversary_filter_df["Name"]+"----"+anniversary_filter_df["Spouse"]
+
+    return birthday_filter_df["Name"],anniversary_filter_df["pair_name"]
 
 @app.route("/")
 def index_page():
-    today_date=datetime.date.today().strftime('%d/%m')    
-    return render_template("index.html")
+    birthday_today,anniversary_today=filter_data()   
+    return render_template("index.html",
+                           birthday_today=birthday_today,
+                           anniversary_today=anniversary_today)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
